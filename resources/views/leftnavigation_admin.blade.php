9403f5a232ff65c0a87a424ea348359ccd8b1a6f
Sushumna Khandelwal <sush.94kh@gmail.com>
2016-06-28 12:59:28 +0530

    Logo added

--- resources/views/leftnavigation_admin.blade.php
+++ resources/views/leftnavigation_admin.blade.php
@@ -3,10 +3,10 @@
         <ul class="nav metismenu" id="side-menu">
             <li class="nav-header">
                 <div class="dropdown profile-element"> <span>
-                    <img alt="image" class="img-circle" src="img/profile_small.jpg" />
+                    <img alt="image" src="img/bpcl.png"/>
                 </span>
                 <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                    <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">{{Auth::user()->customer_code}}</strong>
+                    <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">Welcome {{Auth::user()->customer_code}}</strong>
                     </span> <span class="text-muted text-xs block"></span> </span> </a>
 
                 </div>
